d13794830e3df1333309318f3c356955ca636601
Asmar <asmarcd@gmail.com>
2020-12-17 17:37:46 -0800

    refactor of back end. routes and controllers next

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,12 +1,8 @@
-const URL_PREFIX = "http://localhost:3001"
-// const URL_PREFIX = "https://wandir.herokuapp.com"
-
 const API = {
     titleSearch: function (titleInput) {
         return fetch(`https://www.googleapis.com/books/v1/volumes?q=${titleInput}+intitle&key=${process.env.REACT_APP_GOOGLE_API_KEY}`)
             .then(response => response.json())
     }
-
 }
 
 module.exports = API;
