851b9a708c5ca83bc75100a8739ab5c1c7baca6d
Asmar <asmarcd@gmail.com>
2020-12-18 13:17:23 -0800

    api built

--- src/components/saved.js
+++ src/components/saved.js
@@ -1,6 +1,9 @@
-
+import React from 'react';
+import API from '../utils/API'
 
 const Saved = () => {
+
+
     return (
         <p>Saved placeholder</p>
     )
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,18 @@
+import axios from 'axios'
+
 const API = {
     titleSearch: function (titleInput) {
         return fetch(`https://www.googleapis.com/books/v1/volumes?q=${titleInput}+intitle&key=${process.env.REACT_APP_GOOGLE_API_KEY}`)
             .then(response => response.json())
+    },
+    getAllBooks: function () {
+        API.get('/books')
+    },
+    saveBook: function (newBook) {
+        API.post('/book', newBook)
+    },
+    deleteBook: function (id) {
+        API.deleteBook(`/book/${id}`)
     }
 }
 
