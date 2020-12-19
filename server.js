2b365986e300817958984141345eaa2a5a94507b
Asmar <asmarcd@gmail.com>
2020-12-19 09:18:44 -0800

    express adjustments

--- server.js
+++ server.js
@@ -1,30 +1,20 @@
 const express = require("express");
-const logger = require("morgan");
+const path = require('path');
 const mongoose = require("mongoose");
-const compression = require("compression");
-
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const path = require('path')
-
-const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger("dev"));
-
-app.use(compression());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
-app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/', (req,res) => {
+  return "Hello"
+});
 
 app.get('*', (req,res) =>{
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
+
+const PORT = process.env.PORT || 3000;
 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
