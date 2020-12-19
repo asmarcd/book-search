e082da77333e6640443270b7532abbc33429edf6
Asmar <asmarcd@gmail.com>
2020-12-18 20:33:16 -0800

    heroku refactor

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path')
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbExample", { useNewUrlParser: true });
 
 const db = require('./db')
 const bookRouter = require('./routes/book-router')
 
 const app = express()
-const apiPort = 3000
+const apiPort = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
+app.use(express.static(path.join(_dirname, "client", "build")))
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
 app.use('/api', bookRouter)
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(_dirname, "client", "build", "index.html"));
+});
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
